e7ab528edd88e285da74f7785f4a9059f8049c28
Костянтин Кубряк <konstantin.kubriak@gmail.com>
2025-07-12 20:28:18 +0300

    docs: add Jest+Babel audit and config patches

--- server/babel.config.cjs
+++ server/babel.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('@babel/core').TransformOptions} */
 module.exports = {
-
+  root: true,
+  babelrc: false,
   presets: [
     ['@babel/preset-env', { targets: { node: '20' } }],
     '@babel/preset-typescript'
